3a0d38b02efdad805d6b6060d940ee348ea26b9f
Ravi Assis <ravi.g.assis@gmail.com>
2021-12-31 09:59:13 -0300

    State pattern

--- design-patterns/src/main/java/br/com/alura/loja/orcamento/Orcamento.java
+++ design-patterns/src/main/java/br/com/alura/loja/orcamento/Orcamento.java
@@ -1,7 +1,10 @@
 package br.com.alura.loja.orcamento;
 
+import br.com.alura.loja.orcamento.situacao.EmAnalise;
+import br.com.alura.loja.orcamento.situacao.SituacaoOrcamento;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 
@@ -11,4 +14,25 @@ public class Orcamento {
     private BigDecimal valor;
     @Getter
     private int quantidadeItens;
+
+    @Getter
+    @Setter
+    private SituacaoOrcamento situacao = new EmAnalise();
+
+    public void aplicarDescontoExtra(){
+        var valorDescontoExtra = this.situacao.calcularDescontoExtra(this);
+        this.valor = this.valor.subtract(valorDescontoExtra);
+    }
+
+    public void aprovar() {
+        this.situacao.aprovar(this);
+    }
+
+    public void reprovar() {
+        this.situacao.reprovar(this);
+    }
+
+    public void finalizar() {
+        this.situacao.finalizar(this);
+    }
 }
---
+++ design-patterns/src/main/java/br/com/alura/loja/orcamento/situacao/Aprovado.java
@@ -0,0 +1,18 @@
+package br.com.alura.loja.orcamento.situacao;
+
+import br.com.alura.loja.DomainException;
+import br.com.alura.loja.orcamento.Orcamento;
+
+import java.math.BigDecimal;
+
+public class Aprovado extends SituacaoOrcamento{
+    @Override
+    public BigDecimal calcularDescontoExtra(Orcamento orcamento) {
+        return orcamento.getValor().multiply(new BigDecimal("0.02"));
+    }
+
+    @Override
+    public void finalizar(Orcamento orcamento) {
+        orcamento.setSituacao(new Finalizado());
+    }
+}
